ac32dea62179e1f68902333035a1b8a97497030c
Carlos <crarana@fullsail.edu>
2015-04-24 22:52:53 -0400

    updated
    updated duel

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -10,30 +10,34 @@
 
 //Variables and functions being declared
 
-//Arrays for players, playeronedamage, and playeronehealth
+//Objects for fighters 
 
-	var fighter1 = ["Spiderman", 20, 100]; //array for fighter1
-	var fighter2 = ["Batman", 20, 100]; //array for fighter2
+	var fighter1 = {name:"Spiderman", damage:20, health:100}; //array for fighter1
+	var fighter2 = {name:"Batman", damage:20, health:100}; //array for fighter2
 	
+	//declares first round
 	var round = 1;
-	//this is the fight function
+	
+	//gets the element in #round_number and changes the html in it to the following
 	document.getElementById("round_number").innerHTML = '<h2>Click To Start Fight</h2>';
+	//gets the element #fight_btn and gives it an onclick method that triggers the fight function
 	document.getElementById("fight_btn").onclick = fight;
 	
+	//this is the fight function
 	function fight(){
 
 			//random formula is - Math.floor(Math.random() * (max-min) + min);
-			var minDamage1 = fighter1[1] * .5; //math equation for mindamage for fighter1
-			var minDamage2 = fighter2[1] * .5; //math equation for mindamage for fighter2
-			var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1); //math equation  for random damage for fighter1
-			var f2 = Math.floor(Math.random()*(fighter2[1]-minDamage2)+minDamage2); //math equation for random damage for fighter2
+			var minDamage1 = fighter1.damage * .5; //math equation for mindamage for fighter1
+			var minDamage2 = fighter2.damage * .5; //math equation for mindamage for fighter2
+			var f1 = Math.floor(Math.random()*(fighter1.damage-minDamage1)+minDamage1); //math equation  for random damage for fighter1
+			var f2 = Math.floor(Math.random()*(fighter2.damage-minDamage2)+minDamage2); //math equation for random damage for fighter2
 		
 			//inflicting damage 
-			fighter1[2] -= f1; //inflicting damage to fighter1's health
-			fighter2[2] -= f2; //inflincting damage to fighter2's health
-			document.getElementById("round_number").innerHTML = '<h2>Round' + ' ' + round + ' ' + 'Complete!!</h2>';
-			document.getElementById("kabal").innerHTML = '<p>' + fighter1[0] + ':'+ ' ' + fighter1[2] + '</p>';
-			document.getElementById("kratos").innerHTML = '<p>' + fighter2[0] + ':'+ ' ' + fighter2[2] + '</p>';
+			fighter1.health -= f1; //inflicting damage to fighter1's health
+			fighter2.health -= f2; //inflincting damage to fighter2's health
+			document.getElementById("round_number").innerHTML = '<h2>Round' + ' ' + round + ' ' + 'Complete!!</h2>'; //change the html of round_number to display the current roung
+			document.getElementById("kabal").innerHTML = '<p>' + fighter1.name + ':'+ ' ' + fighter1.health + '</p>'; //change the html of fighter 1 to display current round
+			document.getElementById("kratos").innerHTML = '<p>' + fighter2.name + ':'+ ' ' + fighter2.health + '</p>'; //change the html of fighter 2 to display current round
 			
 			//check for winner with the results of current combat round
 			var results = winnerCheck(); 			
@@ -44,7 +48,6 @@
 			
 				//round increment
 			}else{
-				//breaks if the rounds finish due to lack of health for 
 				
 			};
 			
@@ -57,23 +60,23 @@
 		var result="no winner";
 		
 		//conditional statement which checks for a winner
-		if(fighter1[2]<1&&fighter2[2]<1){ //if fighter ones health, stored in index 2, is less than 1 && fighter twos health, stored in index 2, is less than 1
-			document.getElementById("scores").innerHTML = '<p style="text-align: center;padding: 0 auto">You both lose!</p>';
-			document.getElementById("fight_btn").onclick = function(e){
-					e.preventDefault();
-				return false;
+		if(fighter1.health<1&&fighter2.health<1){ //if fighter ones health, stored in index 2, is less than 1 && fighter twos health, stored in index 2, is less than 1
+			document.getElementById("scores").innerHTML = '<p style="text-align: center;padding: 0 auto">You both lose!</p>'; //overrides score to display no winner, both lose.
+			document.getElementById("fight_btn").onclick = function(e){ //calls the function that is triggered by the click event and changes it
+					e.preventDefault(); //by preventing its default
+				return false; // and stopping its action
 			};
-		}else if(fighter1[2]<1){ //if fighter1's health is less than 1, 
-			document.getElementById("scores").innerHTML = '<p style="text-align: center;padding: 0 auto">' + fighter2[0] + ' ' +'wins!</p>';
-			document.getElementById("fight_btn").onclick = function(e){
-					e.preventDefault();
-				return false;
+		}else if(fighter1.health<1){ //if fighter1's health is less than 1, 
+			document.getElementById("scores").innerHTML = '<p style="text-align: center;padding: 0 auto">' + fighter2.name + ' ' +'wins!</p>'; //overrides score to display fighter2 as winner
+			document.getElementById("fight_btn").onclick = function(e){ //calls the function that is triggered by the click event and changes it
+					e.preventDefault(); //by preventing its default
+				return false; // and stopping its action
 			};
-		}else if(fighter2[2]<1){ //if fighter2' health is less than 1,
-			document.getElementById("kabal").innerHTML = '<p style="text-align: center;padding: 0 auto">' + fighter1[0] + ' ' + 'wins!</p>';
-			document.getElementById("fight_btn").onclick = function(e){
-					e.preventDefault();
-				return false;
+		}else if(fighter2.health<1){ //if fighter2' health is less than 1,
+			document.getElementById("kabal").innerHTML = '<p style="text-align: center;padding: 0 auto">' + fighter1.name + ' ' + 'wins!</p>'; //overrides score to display fighter1 as winner.
+			document.getElementById("fight_btn").onclick = function(e){ //calls the function that is triggered by the click event and changes it
+					e.preventDefault(); //by preventing its default
+				return false; // and stopping its action
 			};
 		};
 		//sets result for winnerCheck function
